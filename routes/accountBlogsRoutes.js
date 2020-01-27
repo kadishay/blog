f22b35ab8f45472635cb22e3689db1df482dbff2
Yotam <yotam.kadishay@gmail.com>
2020-01-27 16:51:58 +0200

    create account user

--- routes/accountBlogsRoutes.js
+++ routes/accountBlogsRoutes.js
@@ -1,6 +1,9 @@
 const Router = require('express').Router;
 const db = require('../db');
 
+const mongodb = require('mongodb');
+const ObjectId = mongodb.ObjectId;
+
 const router = Router();
 
 router.get('/all', (req, res, next) => {
@@ -24,11 +27,11 @@ router.get('/all', (req, res, next) => {
 
 router.post('/create', (req, res, next) => {
   const new_account = {
-    creation_date: Date.now(),
+    creation_date: new Date(Date.now()).toISOString(),
     blogs: [],
     creator: {
       name: req.body.creator_name,
-      name: req.body.creator_email
+      email: req.body.creator_email
     }, 
     users: []
   };
@@ -45,8 +48,45 @@ router.post('/create', (req, res, next) => {
     .catch(err => {
       console.log(err);
       res
-        .status(500)
-        .json({ message: 'An error occurred.' });
+        .status(500).json({ message: 'An error occurred.' });
+    });
+});
+
+router.post('/:id/create-user', (req, res, next) => {
+  console.log(req.body)
+  const new_user = {
+    name: req.body.name,
+    email: req.body.mail
+  };
+  db.getDb()
+    .db()
+    .collection('users')
+    .insertOne(new_user)
+    .then(result => {
+      console.log('new user id: ' + result.insertedId);
+      db.getDb()
+        .db()
+        .collection('accounts')
+        .updateOne(
+          { _id: new ObjectId(req.params.id) },
+          { $push: { users: result.insertedId } }
+        )
+        .then(result => {
+          console.log('new user added');
+          res
+            .status(201)
+            .json({ message: 'User created', accountId: result.insertedId});
+        })
+        .catch(err => {
+          console.log(err);
+          res
+            .status(500).json({ message: 'An error occurred.' });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500).json({ message: 'An error occurred.' });
     });
 });
 
